f06ac721921612d8d92e457a35977d4df9440fb3
alhyderroman <alhyderroman807@yahoo.com>
2024-04-26 19:41:20 +0600

    message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express=require ('express');
+const express=require('express');
 const cors=require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -7,8 +7,15 @@ const port=process.env.PORT||5000;
 
 // middleware
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+const corsConfig={
+  origin:['http://localhost:5173',"http://localhost:5174"],
+  credentials:true,
+  methods:['GET','POST','PUT','DELETE']
+}
+app.use(cors(corsConfig))
+app.options('',cors(corsConfig))
+  app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kxgqdcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -26,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const coffeeCollection=client.db('coffeeDB').collection('coffee');
     const userCollection=client.db('coffeeDB').collection('user');
     //send all data to client to show to UI
@@ -113,7 +120,7 @@ async function run() {
     res.send(result);
     })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
